9418e5e57b7c417cbc40ee9959a164269de22d41
Divine power <jiang_P_T@hotmail.com>
2019-10-27 17:28:05 +0800

    写下流程

--- app/src/main/java/com/example/divinepower/MainActivity.java
+++ app/src/main/java/com/example/divinepower/MainActivity.java
@@ -8,7 +8,11 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //在这里 new 一个 视图 适合所有屏幕的
+        //todo
+        //下面把视图加载
+//        setContentView(新视图);
         setContentView(R.layout.activity_main);
-        //=========
+        //===========
     }
 }
