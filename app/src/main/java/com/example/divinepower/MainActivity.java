323578a86b2cbd58747e847508e93ce21add6d23
DivinePower <jiang_P_T@hotmail.com>
2019-10-29 20:04:21 +0800

    测试

--- app/src/main/java/com/example/divinepower/MainActivity.java
+++ app/src/main/java/com/example/divinepower/MainActivity.java
@@ -14,5 +14,6 @@ public class MainActivity extends AppCompatActivity {
 //        setContentView(新视图);
         setContentView(R.layout.activity_main);
         //===========
+        //测试
     }
 }
