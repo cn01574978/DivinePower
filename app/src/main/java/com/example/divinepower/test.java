7db423cdfa5c7dfd6aa4409296e6813d13f48b84
DivinePower <jiang_P_T@hotmail.com>
2019-10-29 20:12:00 +0800

    测试类

---
+++ app/src/main/java/com/example/divinepower/test.java
@@ -0,0 +1,14 @@
+package com.example.divinepower;
+
+/**
+ * Copyright (C), 2015-2019, XXX有限公司
+ * FileName: test
+ * Author: jpt-009
+ * Date: 2019/10/29/029 20:11:31
+ * Description: ${DESCRIPTION}
+ * History:
+ * <author> <time> <version> <desc>
+ * 9 * 作者姓名 修改时间 版本号 描述
+ */
+public class test {
+}
